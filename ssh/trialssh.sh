3649182a35fac0adf47f1d09491c6f176c80eb1c
llowq <96781765+kenDevXD@users.noreply.github.com>
2023-01-24 23:58:41 +0700

    Update trialssh.sh

--- ssh/trialssh.sh
+++ ssh/trialssh.sh
@@ -1,55 +1,79 @@
 #!/bin/bash
-# ==========================================
-# Getting
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-domain=$(cat /root/scdomain)
+echo "Checking VPS"
 clear
-read -p "Username : " Login
-read -p "Expired (Days): " masaaktif
-
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
 IP=$(wget -qO- ipinfo.io/ip);
-ws="$(cat ~/log-install.txt | grep -w "Websocket TLS" | cut -d: -f2|sed 's/ //g')"
-ws2="$(cat ~/log-install.txt | grep -w "Websocket None TLS" | cut -d: -f2|sed 's/ //g')"
-ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+MYIP=$(wget -qO- ipinfo.io/ip);
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+hari="1"
+Pass=1
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
 clear
-random=`</dev/urandom tr -dc X-Z0-9 | head -c4`
-Pass=$Login$random
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
+clear
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-hariini=`date -d "0 days" +"%Y-%m-%d"`
-expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e ""
-echo -e "═══════════════════════"
-echo -e "Informasi Trial SSH Premium"
-echo -e "═══════════════════════"
-echo -e "Hostname    : $domain"
-echo -e "Username    : $Login"
+echo -e "==============================="
+echo -e "Informasi SSH & OpenVPN"
+echo -e "==============================="
+echo -e "IP/Host     : $IP"
+echo -e "Domain      : ${domain}"
+echo -e "Username    : $Login "
 echo -e "Password    : $Pass"
-echo -e "═══════════════════════"
+echo -e "==============================="
 echo -e "OpenSSH     : 22"
-echo -e "Dropbear    : 443, 109, 143"
-echo -e "SSL/TLS     :$ssl"
-echo -e "═══════════════════════"
-echo -e "OHP SSH     : 8181"
-echo -e "OHP Dropbear: 8282"
-echo -e "═══════════════════════"
-echo -e "SSH Ws SSL  : 443"
-echo -e "SSH Ws HTTP : 80, 8080"
-echo -e "═══════════════════════"
-echo -e "BadVPN      : 7100,7200,7300"
-echo -e "═══════════════════════"
-echo -e "PAYLOAD WS HTTP"
-echo -e "═══════════════════════"
-echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "═══════════════════════"
-echo -e "PAYLOAD WS SSL"
-echo -e "═══════════════════════"
-echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: ${domain}[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "═══════════════════════"
-echo -e "Created     : $hariini"
-echo -e "Expired     : $expi"
-echo -e "═══════════════════════"
-sleep 2
-systemctl restart ssh-ohp
-systemctl restart dropbear-ohp
+echo -e "Dropbear    : 109, 143"
+echo -e "Ws None TLS : 80"
+echo -e "Ws OVPN     : 80"
+echo -e "Ws TLS      : 443"
+echo -e "SSL/TLS     : 222, 777 ,443"
+echo -e "Port Squid  : 3128, 8000"
+echo -e "Port TCP    : 1194"
+echo -e "Port UDP    : 1194"
+echo -e "Port SSL    : 442"
+echo -e "BadVpn      : 7100-7300"
+echo -e "==============================="
+echo -e "PAYLOAD WS : "
+echo -e "==============================="
+echo -e "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "==============================="
+echo -e "PAYLOAD WSTLS : "
+echo -e "================================"
+echo -e "GET wss://Bug.com/ HTTP/1.1[crlf]Host: ${domain}[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "==============================="
+echo -e "Link Download Ovpn"
+echo -e "==============================="
+echo -e "http://$IP:81/tcp.ovpn"
+echo -e "http://$IP:81/udp.ovpn"
+echo -e "http://$IP:81/ssl.ovpn"
+echo -e ""
+echo -e "==============================="
+echo -e "Created  : $created"
+echo -e "Expired   : $exp"
+echo -e "==============================="
+echo -e ""
+echo -e "Script By Kenn XV"
+echo -e ""
